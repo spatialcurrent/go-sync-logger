e4155c009df98dda3a25a40bb23864b360c1b962
pjdufour <pjdufour.dev@gmail.com>
2019-04-01 10:06:39 -0700

    CreateApplicationLogger function for creating typical application logger

---
+++ gsl/CreateApplicationLogger.go
@@ -0,0 +1,80 @@
+// =================================================================
+//
+// Copyright (C) 2019 Spatial Current, Inc. - All Rights Reserved
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
+
+package gsl
+
+import (
+	"fmt"
+	"os"
+)
+
+import (
+	"github.com/pkg/errors"
+)
+
+import (
+	"github.com/spatialcurrent/go-reader-writer/grw"
+)
+
+// CreateApplicationLogger creates a new *Logger given the fields in *CreateApplicationLoggerInput.
+// This function creates a logger that intuitively works as you would expect an application logger to work.
+// The logger shares a single grw.ByteWriteCloser if error and info messages are going to the same location.
+// If verbose mode is on, warn messages are sent to the error log and debug messages are sent to the info log.
+// If there is an error during creation then the program prints the error and exits with exit code 1.
+func CreateApplicationLogger(input *CreateApplicationLoggerInput) *Logger {
+
+	errorWriter, err := grw.WriteToResource(input.ErrorDestination, input.ErrorCompression, true, nil)
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "error creating error writer"))
+		os.Exit(1)
+	}
+
+	levels := map[string]int{"error": 0, "fatal": 0}
+	writers := []grw.ByteWriteCloser{errorWriter}
+	formats := []string{input.ErrorFormat}
+
+	if input.Verbose {
+		levels["warn"] = 0
+	}
+
+	if len(input.InfoDestination) > 0 && input.InfoDestination != "/dev/null" && input.InfoDestination != "null" {
+		if input.InfoDestination == input.ErrorDestination {
+			if input.InfoFormat != input.ErrorFormat {
+				errorWriter.WriteError(fmt.Errorf("info-format ( %s ) and error-format ( %s ) must match when they share a destination", input.InfoFormat, input.ErrorFormat)) // #nosec
+				errorWriter.Close()                                                                                                                                            // #nosec
+				os.Exit(1)
+			}
+			if input.InfoCompression != input.ErrorCompression {
+				errorWriter.WriteError(fmt.Errorf("info-compression ( %s ) and error-compression ( %s ) must match when they share a destination", input.InfoCompression, input.ErrorCompression)) // #nosec
+				errorWriter.Close()                                                                                                                                                                // #nosec
+				os.Exit(1)
+			}
+
+			levels["info"] = 0
+			if input.Verbose {
+				levels["debug"] = 0
+			}
+		} else {
+			infoWriter, err := grw.WriteToResource(input.InfoDestination, input.InfoCompression, true, nil)
+			if err != nil {
+				errorWriter.WriteError(errors.Wrap(err, "error creating log writer")) // #nosec
+				errorWriter.Close()                                                   // #nosec
+				os.Exit(1)
+			}
+
+			levels["info"] = 1
+			writers = append(writers, infoWriter)
+			formats = append(formats, input.InfoFormat)
+
+			if input.Verbose {
+				levels["debug"] = 1
+			}
+		}
+	}
+
+	return NewLogger(levels, writers, formats)
+}
